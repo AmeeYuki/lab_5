304db3cc7334036b0d716f6005384d34f750b859
AmeeYuki <yukinooame@gmail.com>
2023-10-31 22:30:11 +0700

    first lab 5

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="info-page">
+      <h1>My Contacts</h1>
+      <section>
+        <p>
+          Hi, I'm Yuu Mee. I'm a software developer passionate about creating
+          web applications. This is my introduction page.
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default Contact;
